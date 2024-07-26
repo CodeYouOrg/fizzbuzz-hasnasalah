9749ffd5cb0458a804870ca17d460bfded253977
hasnasalah <benjillali.hasna@gmail.com>
2024-07-26 12:59:17 -0400

    corrected

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,7 +1,11 @@
 # add your code here
+
+
 for i in range(1,101):
-    if i%5==0:
-        print("Bizz")
+    if i%5==0 and i%3==0:
+         print("FizzBuzz")
+    elif i%5==0:
+        print("Buzz")
     elif i%3==0:
         print("Fizz")
     else:
